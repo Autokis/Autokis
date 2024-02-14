7eb8fddb6ff7fd6edd8f09495964dd657b1ac8c8
OstapMelnychuk <ostapss222@gmail.com>
2024-02-14 16:00:21 +0200

    initial commit

---
+++ src/main/java/com/example/autokis/AutokisApplication.java
@@ -0,0 +1,13 @@
+package com.example.autokis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutokisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutokisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/autokis/AutokisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autokis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutokisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
