d5a9e057eb865f97eec94b593005835051d906f2
OstapMelnychuk <ostapss222@gmail.com>
2024-02-27 02:30:29 +0200

    AUT-42: Added tests for DDApiDataProviderImpl

--- src/main/java/com/example/autokis/provider/data/ddtuning/DDApiDataProviderImpl.java
+++ src/main/java/com/example/autokis/provider/data/ddtuning/DDApiDataProviderImpl.java
@@ -5,6 +5,7 @@ import com.example.autokis.openapi.model.DDResponse;
 import com.example.autokis.openapi.model.Product;
 import com.example.autokis.provider.ApiDataProvider;
 import com.example.autokis.config.ApiTokenConfiguration;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -24,7 +25,13 @@ public class DDApiDataProviderImpl implements ApiDataProvider {
         List<Product> results = new ArrayList<>();
 
         for (int i = 0; i < 60000; i += 10000) {
-            DDResponse response = ddTuningFeignClient.getAllProducts(i, apiTokenConfiguration.getDdTuningApiToken());
+            DDResponse response;
+            try {
+                response = ddTuningFeignClient.getAllProducts(i, apiTokenConfiguration.getDdTuningApiToken());
+            } catch (FeignException e) {
+                log.error("Error when parsing DDTuning Api with offset %d".formatted(i), e);
+                continue;
+            }
             if (response == null) {
                 continue;
             }
---
+++ src/test/java/com/example/autokis/provider/data/ddtuning/DDApiDataProviderImplTest.java
@@ -0,0 +1,70 @@
+package com.example.autokis.provider.data.ddtuning;
+
+import com.example.autokis.client.feign.DDTuningFeignClient;
+import com.example.autokis.config.ApiTokenConfiguration;
+import com.example.autokis.openapi.model.DDResponse;
+import com.example.autokis.openapi.model.Product;
+import feign.FeignException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class DDApiDataProviderImplTest {
+    @InjectMocks
+    private DDApiDataProviderImpl dataProvider;
+    @Mock
+    private DDTuningFeignClient ddTuningFeignClient;
+    @Mock
+    private ApiTokenConfiguration apiTokenConfiguration;
+
+    @BeforeEach
+    public void setUp() {
+        when(apiTokenConfiguration.getDdTuningApiToken()).thenReturn("test");
+    }
+
+    @Test
+    public void getAllProductsFromAPI() {
+        DDResponse response = prepareResponse();
+
+        when(ddTuningFeignClient.getAllProducts(anyInt(), eq("test"))).thenReturn(response);
+
+        List<Product> allProductsFromAPI = dataProvider.getAllProductsFromAPI();
+        assertEquals(6, allProductsFromAPI.size());
+    }
+
+    @Test
+    public void getAllProductsFromApiFeignClientReturnNull() {
+        when(ddTuningFeignClient.getAllProducts(anyInt(), eq("test"))).thenReturn(null);
+
+        List<Product> allProductsFromAPI = dataProvider.getAllProductsFromAPI();
+        assertEquals(0, allProductsFromAPI.size());
+    }
+
+    @Test
+    public void getAllProductsFromApiFeignClientThrowsException() {
+        when(ddTuningFeignClient.getAllProducts(anyInt(), eq("test"))).thenThrow(FeignException.NotFound.class);
+
+        List<Product> allProductsFromAPI = dataProvider.getAllProductsFromAPI();
+        assertEquals(0, allProductsFromAPI.size());
+    }
+
+    private DDResponse prepareResponse() {
+        DDResponse response = new DDResponse();
+        Product product = new Product();
+        product.setId(1);
+        product.setCategory("test");
+        product.setTitle("test");
+        response.data(List.of(product));
+        return response;
+    }
+}
