0df55a91608192e3fcaf7dac4707311ddbc2ef2a
Ostap Melnychuk <40043747+OstapMelnychuk@users.noreply.github.com>
2024-02-28 22:59:40 +0200

    AUT-42: Get products from DDTuning (#8)
    * AUT-42: Added FeignClient. Added classes to get Products from DDTuning. Created basic infrastructure for Product controller via OpenApi
    
    * AUT-42: Removed unused dependency. Removed unused property in yaml file
    
    * AUT-42: Added tests for DDApiDataProviderImpl
    
    * AUT-42: Removed Sonar issues
    
    * AUT-42: Added maven profile to report sonar test covering
    
    * AUT-42: Fix testing coverage
    
    * AUT-42: Fixed due to github comments
    
    * AUT-42: Fixed Sonar issues
    
    * AUT-42: Fixed Sonar issues 2
    
    * AUT-42: Merged with develop
    
    * AUT-42: Fixed cyrilic c
    
    * AUT-42: Removed unused property in application.properties
    
    * Update README.md
    
    * AUT-42: Fixed checkstyle.xml

---
+++ boot/src/main/java/ua/com/autokis/boot/service/ProductServiceImpl.java
@@ -0,0 +1,21 @@
+package ua.com.autokis.boot.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import ua.com.autokis.boot.provider.ApiDataProvider;
+import ua.com.autokis.openapi.api.ProductsApiDelegate;
+import ua.com.autokis.openapi.model.Product;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductsApiDelegate {
+    private final ApiDataProvider dataProvider;
+    @Override
+    public ResponseEntity<List<Product>> getAllProducts() {
+        //Later would use db call
+        return ResponseEntity.ok().body(dataProvider.getAllProductsFromAPI());
+    }
+}
