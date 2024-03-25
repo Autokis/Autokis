8fdcadc489d291091f2cc4b319eced45db67faf6
OstapMelnychuk <ostapss222@gmail.com>
2024-03-25 13:06:47 +0200

    AUT-63: Added new endpoint to search reviews by productId

--- application/src/main/java/ua/com/autokis/application/repository/review/ReviewRepository.java
+++ application/src/main/java/ua/com/autokis/application/repository/review/ReviewRepository.java
@@ -2,8 +2,12 @@ package ua.com.autokis.application.repository.review;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ua.com.autokis.application.entity.ProductEntity;
 import ua.com.autokis.application.entity.review.ReviewEntity;
+
+import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Integer> {
+    List<ReviewEntity> findByProduct(ProductEntity product);
 }
